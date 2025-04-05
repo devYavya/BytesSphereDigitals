6c712c762e7a620fa0a285bf92debd28a14272a5
devYavya <yavya.sharma21@gmail.com>
2025-04-05 11:22:17 +0530

    SEO

--- Your-Dev-Friend/generate-sitemap.cjs
+++ Your-Dev-Friend/generate-sitemap.cjs
@@ -1,7 +1,31 @@
 const SitemapGenerator = require("sitemap-generator");
+const path = require("path");
+const fs = require("fs");
 
-const generator = SitemapGenerator("https://bytespheredigital.com", {
-  stripQuerystring: false,
+const siteUrl = "https://bytespheredigital.com";
+
+const outputFolder = path.resolve(__dirname, "public");
+
+if (!fs.existsSync(outputFolder)) {
+  fs.mkdirSync(outputFolder, { recursive: true });
+}
+
+const generator = SitemapGenerator(siteUrl, {
+  stripQuerystring: true,
+  filepath: path.join(outputFolder, "sitemap.xml"),
+  maxDepth: 0,
+});
+
+generator.on("add", (url) => {
+  console.log(`Added: ${url}`);
+});
+
+generator.on("done", () => {
+  console.log("Sitemap generation completed!");
+});
+
+generator.on("error", (error) => {
+  console.error("Error:", error);
 });
 
 generator.start();
